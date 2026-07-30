58df4c1f17f509dbbdbeba8c7924a26f36efbc67
Daniyal9988 <sdani9988@gmail.com>
2026-07-31 00:44:18 +0100

    ADDED DATABSE CRED DIRECTLY

--- db.js
+++ db.js
@@ -3,13 +3,13 @@ const mysql = require('mysql2');
 
 // MySQL Database Connection Pool using environment variables
 const db = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,       
-    database: process.env.DB_NAME,
+    host: process.env.DB_HOST || '50.6.43.53',
+    user: process.env.DB_USER || 'mxkdpnmy_dany',
+    password: process.env.DB_PASSWORD || 'DanyGseSql2021',        
+    database: process.env.DB_NAME || 'mxkdpnmy_gse_database',
+    port: process.env.DB_PORT || 3306,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
 });
-
 module.exports = db.promise();
